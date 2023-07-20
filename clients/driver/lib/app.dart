b65e6d2ebe8741565a4b8c03071d28b501c24409
khangduy017 <khangduy017@gmail.com>
2023-07-20 12:48:30 +0700

    set up git driver app

---
+++ clients/driver/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('Driver App!'),
+      ),
+    );
+  }
+}
