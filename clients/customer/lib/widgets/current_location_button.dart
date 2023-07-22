4b7d81c30e830738bfd93365c28d37d2c7f0ba8c
khangduy017 <khangduy017@gmail.com>
2023-07-22 11:56:04 +0700

    add current location button

---
+++ clients/customer/lib/widgets/current_location_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CurrentLocationButton extends StatelessWidget {
+  const CurrentLocationButton({
+    Key? key,
+    required this.getCurrentLocation,
+  }) : super(key: key);
+
+  final void Function() getCurrentLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(120, 193, 193, 193),
+            spreadRadius: 1,
+            blurRadius: 1,
+            offset: Offset(0, 0),
+          )
+        ],
+      ),
+      padding: EdgeInsets.zero,
+      child: IconButton(
+        onPressed: getCurrentLocation,
+        padding: EdgeInsets.zero,
+        icon: Image.asset('assets/location_button.png'),
+        iconSize: 60,
+      ),
+    );
+  }
+}
