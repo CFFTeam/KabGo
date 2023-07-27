f160363813f9dba66ad0c4897146028051cb2e89
Gia Huy <giahuy200202@gmail.com>
2023-07-27 16:26:36 +0700

    nav bar 1/2

--- clients/admin/src/components/Navbar/index.tsx
+++ clients/admin/src/components/Navbar/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
+import styles from "../../assets/css/Navbar/index.module.css";
+import { ReactComponent as OpenClose } from "../../assets/svg/Navbar/closeopen.svg";
+import { ReactComponent as GoBack } from "../../assets/svg/Navbar/goback.svg";
+import { ReactComponent as Reload } from "../../assets/svg/Navbar/reload.svg";
+import { ReactComponent as User } from "../../assets/svg/Navbar/user.svg";
+import { ReactComponent as Notification } from "../../assets/svg/Navbar/notification.svg";
+import { ReactComponent as Setting } from "../../assets/svg/Navbar/setting.svg";
+import { ReactComponent as Logout } from "../../assets/svg/Navbar/logout.svg";
 
 const Navbar: React.FC = () => {
-    return (
-        <nav>
-            <b>Navbar</b>
-        </nav>
-    );
-}
+  return (
+    <nav className={styles["nav-container"]}>
+      <div className={styles["content-container-openclose"]}>
+        <div className={styles["img-container"]}>
+          <OpenClose />
+        </div>
+      </div>
+      <div className={styles["title-container"]}>
+        <div className={styles["sub-title"]}>
+            <GoBack />
+            <Reload />
+            <div className={styles["title"]}>Dashboard</div>
+        </div>
+        <div className={styles["sub-option"]}>
+            <User />
+            <Notification />
+            <Setting />
+            <Logout className={styles["test"]}/>
+        </div>
+      </div>
+      <div className={styles["profile-container"]}>
+
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
