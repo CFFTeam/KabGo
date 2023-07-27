c2c262c5cdd7ce685a7fc3782524dc5867e754bd
Gia Huy <giahuy200202@gmail.com>
2023-07-27 20:35:16 +0700

    done Admin navbar

--- clients/admin/src/layouts/BaseLayout.tsx
+++ clients/admin/src/layouts/BaseLayout.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '@components/Navbar';
-import Sidebar from '@components/Sidebar';
-import styles from '../assets/css/index.module.css'
-// import {ReactComponent as UserIcon} from '../assets/svg/Navbar/user.svg'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "@components/Navbar";
+import Sidebar from "@components/Sidebar";
+import styles from "../assets/css/layout.module.css";
 
 const BaseLayout: React.FC = () => {
-    return <div className={styles["base-layout-container"]}>
-        <Sidebar />
-        <div className={styles["content-container"]}>
-          <Navbar />
-          {/* <UserIcon className={styles['test']}/> */}
-          <Outlet />
-        </div>
+  return (
+    <div className={styles["base-layout-container"]}>
+      <Sidebar />
+      <div className={styles["content-container"]}>
+        <Navbar />
+        <Outlet />
+      </div>
     </div>
-}
+  );
+};
 
 export default BaseLayout;
