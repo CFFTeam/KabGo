b9757635a2fad9d3f77277a0b5b4ab5a8af4638d
Gia Huy <giahuy200202@gmail.com>
2023-07-27 22:04:24 +0700

    each category

--- clients/admin/src/components/Dashboard/index.tsx
+++ clients/admin/src/components/Dashboard/index.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import styles from "../../assets/css/dashboard.module.css";
+import { ReactComponent as User } from "../../assets/svg/Dashboard/user.svg";
+import { ReactComponent as Driver } from "../../assets/svg/Dashboard/driver.svg";
+import { ReactComponent as Trip } from "../../assets/svg/Dashboard/trip.svg";
+import { ReactComponent as CanceledTrip } from "../../assets/svg/Dashboard/canceledtrip.svg";
 
 const Dashboard: React.FC = () => {
   return (
-    <div>
-        Dashboard
+    <div className={styles["dashboard-container"]}>
+      <div className={styles["category-container"]}>
+
+        <div className={styles["category"]}>
+          <div className={styles["category-info"]}>
+            <div className={styles["category-name"]}>2.152</div>
+            <div className={styles["category-role"]}>Người dùng</div>
+          </div>
+          <div className={styles["category-img"]}>
+            <User />
+          </div>
+        </div>
+
+        <div className={styles["category"]}></div>
+        <div className={styles["category"]}></div>
+        <div className={styles["category"]}></div>
+      </div>
     </div>
   );
 };
