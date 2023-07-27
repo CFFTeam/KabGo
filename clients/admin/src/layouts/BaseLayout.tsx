c497d8dc9cf262641b049466da6ee1ac8692f19d
Gia Huy <giahuy200202@gmail.com>
2023-07-27 14:35:18 +0700

    basic layout of dashboard

--- clients/admin/src/layouts/BaseLayout.tsx
+++ clients/admin/src/layouts/BaseLayout.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '@components/Navbar';
-import Footer from '@components/Footer';
+import Sidebar from '@components/Sidebar';
+import styles from '../assets/css/index.module.css'
+// import {ReactComponent as UserIcon} from '../assets/svg/Navbar/user.svg'
 
 const BaseLayout: React.FC = () => {
-    return <React.Fragment>
-        <Navbar />
-        <Outlet />
-        <Footer />
-    </React.Fragment>
+    return <div className={styles["base-layout-container"]}>
+        <Sidebar />
+        <div className={styles["content-container"]}>
+          <Navbar />
+          {/* <UserIcon className={styles['test']}/> */}
+          <Outlet />
+        </div>
+    </div>
 }
 
 export default BaseLayout;
