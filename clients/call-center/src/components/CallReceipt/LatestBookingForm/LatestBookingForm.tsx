259f1178d9726da105fc46cd89cbb21a02947c87
thoaikhoa14402 <nguyenthoaidangkhoa@gmail.com>
2023-08-01 00:57:32 +0700

    finish most visited addresses from

--- clients/call-center/src/components/CallReceipt/LatestBookingForm/LatestBookingForm.tsx
+++ clients/call-center/src/components/CallReceipt/LatestBookingForm/LatestBookingForm.tsx
@@ -1,13 +1,13 @@
 import styles from './LatestBookingForm.module.css';
 
 const LatestBookingForm: React.FC = () => {
-    return <form className={styles["latest-booking-form"]}>
+    return <table className={styles["latest-booking-form"]}>
         <div className = {styles["form-heading"]}>
         <span className={styles["title"]}>
                 Cuốc xe gần đây
             </span>
         </div>
-    </form>
+    </table>
 }
 
 export default LatestBookingForm;
--- clients/call-center/src/components/CallReceipt/MostVisitedAddressForm/MostVisitedAddressForm.tsx
+++ clients/call-center/src/components/CallReceipt/MostVisitedAddressForm/MostVisitedAddressForm.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./MostVisitedAddressForm.module.css";
+import {ReactComponent as LocationIcon} from "@assets/svg/CallReceipt/location.svg";
 
 const MostVisitedAddressForm: React.FC = () => {
     return <form className={styles["most-visited-address-form"]}>
@@ -8,6 +9,95 @@ const MostVisitedAddressForm: React.FC = () => {
                 Địa chỉ đi nhiều nhất
             </span>
         </div>
+        <table className={styles["table-list"]}>
+            <thead className = {styles["table-heading"]}>
+                <tr>
+                    <th>
+                        <div style = {{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                            <span style = {{lineHeight: "1em", fontSize: "1.8rem"}}>
+                                Địa chỉ
+                            </span>
+                            <span className={styles["address-icn"]}>
+                                <LocationIcon style = {{fontSize: "2.4rem", marginLeft: "1rem"}}/>
+                            </span>
+                        </div>
+                    </th>  
+                    <th style = {{fontSize: "1.8rem"}}>Lượt đi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr style = {{backgroundColor: "#FBF9FA", cursor: "pointer"}}>
+                    <td>
+                        <div style = {{display: "flex", gap: "1rem", marginTop: '0.3rem'}}>
+                            <span>
+                                <LocationIcon style = {{fontSize: "2rem"}}/>
+                            </span>
+                            <span style = {{fontSize: "1.7rem"}}>
+                              22/16 Huỳnh Đình Hai, phường 24, quận Bình Thạnh
+                            </span>
+                        </div>
+                    </td>
+                    <td style = {{fontSize: "1.7rem", color: "#F86C1D", fontWeight: "600"}}>5 lần</td>
+                </tr>
+
+                <tr style = {{backgroundColor: "#FBF9FA", cursor: "pointer"}}>
+                    <td>
+                        <div style = {{display: "flex", gap: "1rem", marginTop: '0.3rem'}}>
+                            <span>
+                                <LocationIcon style = {{fontSize: "2rem"}}/>
+                            </span>
+                            <span style = {{fontSize: "1.7rem"}}>
+                              22/16 Huỳnh Đình Hai, phường 24, quận Bình Thạnh
+                            </span>
+                        </div>
+                    </td>
+                    <td style = {{fontSize: "1.7rem", color: "#F86C1D", fontWeight: "600"}}>5 lần</td>
+                </tr>
+
+                <tr style = {{backgroundColor: "#FBF9FA", cursor: "pointer"}}>
+                    <td>
+                        <div style = {{display: "flex", gap: "1rem", marginTop: '0.3rem'}}>
+                            <span>
+                                <LocationIcon style = {{fontSize: "2rem"}}/>
+                            </span>
+                            <span style = {{fontSize: "1.7rem"}}>
+                              22/16 Huỳnh Đình Hai, phường 24, quận Bình Thạnh
+                            </span>
+                        </div>
+                    </td>
+                    <td style = {{fontSize: "1.7rem", color: "#F86C1D", fontWeight: "600"}}>5 lần</td>
+                </tr>
+
+                <tr style = {{backgroundColor: "#FBF9FA", cursor: "pointer"}}>
+                    <td>
+                        <div style = {{display: "flex", gap: "1rem", marginTop: '0.3rem'}}>
+                            <span>
+                                <LocationIcon style = {{fontSize: "2rem"}}/>
+                            </span>
+                            <span style = {{fontSize: "1.7rem"}}>
+                              22/16 Huỳnh Đình Hai, phường 24, quận Bình Thạnh
+                            </span>
+                        </div>
+                    </td>
+                    <td style = {{fontSize: "1.7rem", color: "#F86C1D", fontWeight: "600"}}>5 lần</td>
+                </tr>
+
+                <tr style = {{backgroundColor: "#FBF9FA", cursor: "pointer"}}>
+                    <td>
+                        <div style = {{display: "flex", gap: "1rem", marginTop: '0.3rem'}}>
+                            <span>
+                                <LocationIcon style = {{fontSize: "2rem"}}/>
+                            </span>
+                            <span style = {{fontSize: "1.7rem"}}>
+                              22/16 Huỳnh Đình Hai, phường 24, quận Bình Thạnh
+                            </span>
+                        </div>
+                    </td>
+                    <td style = {{fontSize: "1.7rem", color: "#F86C1D", fontWeight: "600"}}>5 lần</td>
+                </tr>
+            </tbody>    
+        </table>
+        
     </form>
 }
 
