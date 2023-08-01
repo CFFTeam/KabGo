a6dbfddaef3fba45649965179b9d111bac403288
khangduy017 <khangduy017@gmail.com>
2023-08-01 20:58:56 +0700

    add suggestion item, options button

---
+++ clients/customer/lib/widgets/suggestion_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../models/location_model.dart';
+
+class SuggestionItem extends StatelessWidget {
+  const SuggestionItem({Key? key, required this.data}) : super(key: key);
+  final StructuredFormatting data;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 15, top: 7, bottom: 7),
+      // color: Colors.black,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            data.mainText.toString(),
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            data.secondaryText.toString(),
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ],
+      ),
+    );
+  }
+}
