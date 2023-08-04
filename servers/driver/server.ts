ca40f8b2041f696bec28f068d9c2cf4ed653aa92
NG-MinG <wgmin.it@outlook.com>
2023-08-04 14:03:05 +0700

    create fake socket connection

--- servers/driver/server.ts
+++ servers/driver/server.ts
@@ -1,6 +1,7 @@
 import Application from '@common/app';
 import UserController from '@common/controllers/user.controller';
 import dotenv from 'dotenv';
+import { Server, Socket } from 'socket.io';
 
 process.on('uncaughtException', (err: Error) => {
     console.error('Uncaught Exception. Shutting down...');
@@ -22,11 +23,31 @@ const app = new Application({
 
 const server = app.run(4200);
 
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+io.on('connection', (socket: Socket) => { 
+
+    console.log('Client connected');
+
+    setTimeout(() => socket.emit('welcome', 'Welcome to the server'), 3000);
+    
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
 process.on('unhandledRejection', (err: Error) => {
     console.error('Unhandled Rejection. Shutting down...');
     console.error(err.name, err.message, err.stack);
     
     setTimeout(() => { 
+        io.disconnectSockets();
+        io.close();
+
         server.close(() => {
             process.exit(1); // 0 is success, 1 is uncaught exception
         });
