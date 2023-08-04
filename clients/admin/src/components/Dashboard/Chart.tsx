982757db93d593a8f77b67bb2650afa44bd65b62
Gia Huy <giahuy200202@gmail.com>
2023-08-04 14:55:50 +0700

    chart structure

--- clients/admin/src/components/Dashboard/Chart.tsx
+++ clients/admin/src/components/Dashboard/Chart.tsx
@@ -15,38 +15,38 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-// import { Line } from "react-chartjs-2";
-// import  faker from "faker";  
+import { Line } from "react-chartjs-2";
+// import faker from "faker";  
 
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: 'Dataset 1',
-//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//       borderColor: 'rgb(255, 99, 132)',
-//       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//     },
-//     {
-//       label: 'Dataset 2',
-//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//       borderColor: 'rgb(53, 162, 235)',
-//       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-//     },
-//   ],
-// };
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1,2,3,4,5,6,7],
+      borderColor: 'blue',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [1,1,1,1,1,1,1],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 export const options = {
   responsive: true,
@@ -83,7 +83,7 @@ const ChartComponent: React.FC = () => {
           <div className={styles["currency"]}>VNĐ</div>
         </div>
         <div className={styles["chart-revenue"]}>
-          {/* <Line options={options} data={data} />; */}
+          <Line options={options} data={data} />
         </div>
       </div>
     </div>
