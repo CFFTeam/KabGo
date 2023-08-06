9fa01124466ad8a74f334ea691545b2487c6d9c9
Khoa Nguyen <nguyenthoaidangkhoa@gmail.com>
2023-08-07 02:24:54 +0700

    finish  geolocate logic (place infomration form

--- clients/call-center/src/components/CallReceiptHandler/BookingForm/PlaceInfo/PlaceInfo.tsx
+++ clients/call-center/src/components/CallReceiptHandler/BookingForm/PlaceInfo/PlaceInfo.tsx
@@ -2,35 +2,22 @@ import styles from "./PlaceInfo.module.css";
 import { useState, useRef } from "react";
 import {ReactComponent as PickUpIcon} from '@assets/svg/CallReceipt/pick-up.svg';
 import {ReactComponent as LocationIcon} from '@assets/svg/CallReceipt/location.svg';
-import { GoogleMap, Marker, Autocomplete, DirectionsRenderer, useJsApiLoader, useLoadScript } from '@react-google-maps/api';
-import usePlacesAutocomplete, {getGeocode, getLatLng} from "use-places-autocomplete";
-import PlacesAutocompleteInput from "./PlacesAutocompleteInput/PlacesAutocompleteInput";
+import { GoogleMap, Marker, Autocomplete, DirectionsRenderer, useJsApiLoader, InfoWindow } from '@react-google-maps/api';
 import { useAppDispatch, useAppSelector } from "@hooks/ReduxHooks";
+import PlacesAutocompleteInput from "./PlacesAutocompleteInput/PlacesAutocompleteInput";
+import originMarkerIcon from "@assets/svg/CallReceiptHandler/origin-point-icn.svg";
+import destinationMarkerIcon from "@assets/svg/CallReceiptHandler/des-point-icn.svg";
 import mapStyles from "@assets/googleMapStyles/map.json";
 
-interface originLatLng {
-    lat: number;
-    lng: number;
+
+// define global variables (used for setting)
+const HCMUS_LAT_LNG_CENTER = {
+    lat: 10.763352053865761,
+    lng: 106.68228927747572
 }
 
-interface destinationLatLng {
-    lat: number;
-    lng: number;
-}
-
-const center = {
-    lat: 37.7749295,
-    lng: -122.4194155
-}
-
-// const google = window.google; 
-
-
-let originAutocomplete: any  = {};
-let destinationAutocomplete: any  = {};
-
-// used for map style
-const options = {
+// used for customizing google map UI
+const mapOptions = {
     styles: mapStyles,
     zoomControl: false,
     streetViewControl: false,
@@ -38,27 +25,33 @@ const options = {
     fullscreenControl: false,
 }
 
+// customize direction line
 const polylineOptions = {
     polylineOptions: {
-        strokeColor: '#FFA500', // Orange color
+        strokeColor: '#FF6B00', // Orange color
         strokeOpacity: 1,
         strokeWeight: 4,      // Thickness of the line
       },
+    suppressMarkers: true, // Turn off default markers
 }
 
+// customize marker icon
+const markerOptions = {
+    originLocationIcon: originMarkerIcon,
+    destinationLocationIcon: destinationMarkerIcon,
+}
 
-
+// React PlaceInfo Component
 const PlaceInfo: React.FC = () => {
-
     const dispatch = useAppDispatch();
+    // used for detecting of specific marker when hovering
+
+    const [hoveredMarker, setHoveredMarker] = useState<string>('');
+    // retrieve original_latitude, original_longitude & destination_latitude & destination_longitude
     const { originLatLng: originLatLngGlobal, destinationLatLng: destinationLatLngGlobal} 
     = useAppSelector((state) => state.callReceiptHandler);
 
-    console.log('originLatLngGlobal', originLatLngGlobal);
-    console.log('destinationLatLngGlobal', destinationLatLngGlobal);
-
-    const [isMapLoaded, setIsMapsLoaded] = useState(false);
-
+    // calling Google map API service
     const {isLoaded} = useJsApiLoader({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
         libraries: ['places'],
@@ -66,84 +59,17 @@ const PlaceInfo: React.FC = () => {
         region: 'vn',
     })
 
-    
-
-    const originRef = useRef<HTMLInputElement | null>(null);
-    const destinationRef = useRef<HTMLInputElement | null>(null);
-
-    const [origin, setOrigin] = useState<any>('');
-    const [arrival, setArrival] = useState<any>('');
-
-    const [originLatLng, setOriginLatLng] = useState<any>({});
-    const [arrivalLatLng, setArrivalLatLng] = useState<any>({});
-
-    const handleOriginPlaceChange = (place: any) => {
-        if (place) {
-            setOrigin(place.formatted_address);
-            setOriginLatLng({
-                lat: place.geometry.location.lat(),
-                lng: place.geometry.location.lng()
-                }
-            );
-        }
-        
-    };
-
-    const handleArrivalPlaceChange = (place: any) => {
-        if (place) {
-            setArrival(place.formatted_address);
-            setArrivalLatLng({
-                lat: place.geometry.location.lat(),
-                lng: place.geometry.location.lng()
-                }
-            );
-        }
-    };
-
-    // Experimental section
-
-    // const {
-    //     ready, 
-    //     value,
-    //     setValue,
-    //     suggestions: {status, data},
-    //     clearSuggestions
-    // } = usePlacesAutocomplete();
-    
-    // const handleSelect = (description: any) => {
-    //     setValue(description, false);
-    //     clearSuggestions();
-    //   };
-    
-    // Experimental section
-    
-    
-    
-
-    const [map, setMap] = useState(null);
+    // used for get & set result from calling direction service of google map API
     const [directionsResponse, setDirectionsResponse] = useState<any>(null);
+    
+    // used for calculate distance & duration later
     const [distanctace, setDistance] = useState<any>('');
     const [duration, setDuration] = useState<any>('');
    
-
+    // Used to calculate logic related & render the direction of a route based on the starting address and the destination address
     const calculateRoute = async () => {
-        console.log('isLoaded: ', isLoaded);
-        console.log("window.google.maps: ", window.google.maps);
-        // console.log('google.maps: ', google.maps);
         if (window.google.maps) {
            const directionService = new window.google.maps.DirectionsService();
-            // if (originRef.current?.value === '' || destinationRef.current?.value === '') {
-            //     return;
-            // }
-            // eslint-disable-next-line no-undef
-            console.log('google: ', window.google);
-    
-            // const results  = await directionService.route({
-            //     origin: originRef.current?.value as string,
-            //     destination: destinationRef.current?.value as string,
-            //     travelMode: google.maps.TravelMode.DRIVING
-            // })
-
             const results = await directionService.route({
                 origin: new window.google.maps.LatLng(originLatLngGlobal.lat, originLatLngGlobal.lng),
                 destination: new window.google.maps.LatLng(destinationLatLngGlobal.lat, destinationLatLngGlobal.lng), 
@@ -156,111 +82,116 @@ const PlaceInfo: React.FC = () => {
     }
 
 
-    const clearRoute = () => {
-        setDirectionsResponse(null);
-        setDistance('');
-        setDuration('');
-        if (originRef.current) {
-            originRef.current.value = '';
-        }
-        if (destinationRef.current) {
-            destinationRef.current.value = '';
-        }
-    }
+    // return JSX code
 
+    // if the google map API is not finished
     if (!isLoaded) {
-        return <>Loading....</>
+        return <></>
     }
 
+    // if the google map API is finished
     return <div className={styles["wrapper"]}>
-    <form className={styles["call-receipt-form"]}>
-        <div className={styles["form-heading"]}>
-            <span className={styles["title"]}>
-                Thông tin địa điểm
-            </span>
-        </div>
-        <div className={styles["form-body"]}>
-                <div className={styles["flex-container"]}>
-                    <div className={styles["input"]}>
-                        <label htmlFor="departure-address" style = {{display: "flex", gap: "1rem"}}>
-                            <span className={styles["title"]}>
-                                <PickUpIcon style ={{fontSize: "2rem"}}/>
-                            </span>
-                            <span>
-                                Điểm đón
-                            </span>
-                        </label>
-                        {/* <Autocomplete onLoad={(autocomplete) => (originAutocomplete = autocomplete)} 
-                        onPlaceChanged={() => handleOriginPlaceChange(originAutocomplete.getPlace())}>
-                            <input ref = {originRef} name = "departure-address" placeholder = "Nhập địa chỉ đón..." type = "text" className = {styles["departure-address-inp"]}/>
-                        </Autocomplete> */}
+        <form className={styles["geolocation-form"]}>
+            <div className={styles["form-heading"]}>
+                <span className={styles["title"]}>
+                    Thông tin địa điểm
+                </span>
+            </div>
+            <div className={styles["form-body"]}>
+                    <div className={styles["flex-container"]}>
+                        <div className={styles["input"]}>
+                            <label htmlFor="departure-address" style = {{display: "flex", gap: "1rem"}}>
+                                <span className={styles["title"]}>
+                                    <PickUpIcon style ={{fontSize: "2rem"}}/>
+                                </span>
+                                <span>
+                                    Điểm đón
+                                </span>
+                            </label>
+                            <PlacesAutocompleteInput inputStyle = "origin"/>
+                        </div>
 
-                        {/* <div className={styles["places-complete-input"]} style = {{backgroundColor: "black", position: "relative"}}>
-                            <input style = {{width: "100%"}} value = {value} onChange={(e) => setValue(e.target.value)} type = "text" placeholder = "Nhập địa chỉ đón..." />
-                            {status === 'OK' && 
-                                    <div style = {{position: "absolute", zIndex: 99999, width: "100%"}}>
-                                    {data.map(({place_id, description}, index) => (
-                                    <li style = {{listStyle: "none", backgroundColor: "red", fontSize: "1.5rem", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis",
-                                    padding: "1rem", borderBottom: "1px solid black", cursor: "pointer"
-                                    }} 
-                                    key={place_id} value = {description} onClick={() => handleSelect(description)}>
-                                        {description}
-                                    </li>
-                                    ))}
-                                </div>
-                            }            
-                        </div>  */}
+                        <div className={styles["input"]}>
+                            <label htmlFor="arrival-address" style = {{display: "flex", gap: "1rem"}}>
+                                <span className={styles["title"]}>
+                                <LocationIcon style ={{fontSize: "2rem"}}/>
+                                </span>
+                                <span>
+                                Điểm đến
+                                </span>
+                            </label>
+                            <PlacesAutocompleteInput inputStyle = "destination"/>
+                        </div>
 
-                        <PlacesAutocompleteInput inputStyle = "origin"/>
+                        <div className={styles["location-btn"]}>
+                            <button  type = "button" onClick = {calculateRoute}>
+                                Định vị
+                            </button>
+                        </div>
 
                     </div>
 
-                    <div className={styles["input"]}>
-                        <label htmlFor="arrival-address" style = {{display: "flex", gap: "1rem"}}>
-                            <span className={styles["title"]}>
-                               <LocationIcon style ={{fontSize: "2rem"}}/>
-                            </span>
-                            <span>
-                               Điểm đến
-                            </span>
-                        </label>
-                        {/* eslint-disable-next-line no-undef */}
-                        {/* <Autocomplete onLoad={(autocomplete) => (destinationAutocomplete = autocomplete)}
-                        onPlaceChanged={() => handleArrivalPlaceChange(destinationAutocomplete.getPlace())} >
-                            <input ref = {destinationRef} name = "arrival-address" placeholder = "Nhập địa chỉ đến..." type = "text"  className = {styles["arrival-address-inp"]}/>
-                        </Autocomplete> */}
-
-                        <PlacesAutocompleteInput inputStyle = "destination"/>
-
-                    </div>
-
-                    <div className={styles["location-btn"]}>
-                        <button  type = "button" onClick = {calculateRoute}>
-                            Định vị
-                        </button>
-                    </div>
-
-                </div>
-                {/* Google Map box */}
+    {/* ----------------- Google Map box ----------------- */}
                 <div className={styles["geo-map"]}>
                     {/* Display markers, or directions */}
-                   <GoogleMap center = {center} mapContainerStyle = {{width: '100%', height: '100%'}} zoom = {15} 
-                   options = {options}>
-                    <Marker position = {center} />
-                    {directionsResponse && <DirectionsRenderer directions = {directionsResponse} options = {polylineOptions}/>}
+                    <GoogleMap 
+                    center = {HCMUS_LAT_LNG_CENTER} 
+                    mapContainerStyle = {{width: '100%', height: '100%'}} 
+                    zoom = {10} 
+                    options = {mapOptions}
+                    >
+                        {directionsResponse && 
+                            <>
+                                <DirectionsRenderer directions = {directionsResponse} options = {polylineOptions}/>
+                                <Marker 
+                                position={{
+                                    lat: directionsResponse.routes[0].legs[0].start_location.lat(),
+                                    lng: directionsResponse.routes[0].legs[0].start_location.lng(),
+                                }}
+                                icon={markerOptions.originLocationIcon} 
+                                onMouseOver={() => setHoveredMarker('origin')}
+                                onMouseOut={() => setHoveredMarker('')}
+                                >
+                                    {/* show address when hovering */}
+                                    {hoveredMarker === 'origin' && (
+                                    <InfoWindow>
+                                        <div>{directionsResponse.routes[0].legs[0].start_address}</div>
+                                    </InfoWindow>
+                                    )}
+                                </Marker>
+
+                                {/* destination marker icon */}
+                                <Marker
+                                position={{
+                                    lat: directionsResponse.routes[0].legs[0].end_location.lat(),
+                                    lng: directionsResponse.routes[0].legs[0].end_location.lng(),
+                                    }}
+                                icon={markerOptions.destinationLocationIcon}
+                                onMouseOver={() => setHoveredMarker('destination')}
+                                onMouseOut={() => setHoveredMarker('')}
+                                >
+                                    {/* show address when hovering */}
+                                    {hoveredMarker === 'destination' && (
+                                    <InfoWindow>
+                                        <div>{directionsResponse.routes[0].legs[0].end_address}</div>
+                                    </InfoWindow>
+                                    )}
+                                </Marker>
+                            </>
+                        }
                     </GoogleMap> 
                 </div>
-            <div className={styles["forward-btn"]}>
-                <button className={styles[""]}>
-                    Quay lại
-                </button>
-                <button type = 'submit' >
-                    Tiếp tục
-                </button>
+                <div className={styles["forward-btn"]}>
+                    <button className={styles[""]}>
+                        Quay lại
+                    </button>
+                    <button type = 'submit' >
+                        Tiếp tục
+                    </button>
+                </div>
             </div>
-        </div>
-</form>
-</div>
+        </form>
+    </div>
 }
 
 export default PlaceInfo;
--- clients/call-center/src/components/CallReceiptHandler/BookingForm/PlaceInfo/PlacesAutocompleteInput/PlacesAutocompleteInput.tsx
+++ clients/call-center/src/components/CallReceiptHandler/BookingForm/PlaceInfo/PlacesAutocompleteInput/PlacesAutocompleteInput.tsx
@@ -3,44 +3,14 @@ import { useState } from "react";
 import usePlacesAutocomplete, {getGeocode, getLatLng} from "use-places-autocomplete";
 import { callReceiptHandlerActions } from "@store/reducers/callReceiptHandlerSlice";
 import { useAppDispatch, useAppSelector } from "@hooks/ReduxHooks";
+import {ReactComponent as LocationIcon} from "@assets/svg/CallReceipt/location.svg";
 
 interface PlacesAutocompleteProps {
     inputStyle: "origin" | "destination";
 }
 
 const PlacesAutocomplete = (props: PlacesAutocompleteProps) => {
-    const dispatch = useAppDispatch();
-
-    const handleSelect = async (address: any) => {
-        setValue(address, false);
-        clearSuggestions();
-
-        const results = await getGeocode({address});
-        const {lat, lng} = await getLatLng(results[0]);
-        
-        if (props.inputStyle === "origin")
-        {
-            dispatch(callReceiptHandlerActions.updateOriginGeolocation({
-                lat: lat,
-                lng: lng
-            }))
-            console.log('lat_origin: ', lat);
-            console.log('lng_origin: ', lng);
-        }
-        
-        else if (props.inputStyle === 'destination') {
-            dispatch(callReceiptHandlerActions.updateDestinationGeolocation({
-                lat: lat,
-                lng: lng
-            }))
-            console.log('lat_destination: ', lat);
-            console.log('lng_destination: ', lng);
-        }
-
-
-
-    };
-
+    // define usePlacesAutocomplete
     const {
         ready,
         value,
@@ -52,28 +22,87 @@ const PlacesAutocomplete = (props: PlacesAutocompleteProps) => {
         clearSuggestions
     } = usePlacesAutocomplete();
 
+    const dispatch = useAppDispatch();
+
+    // used to track index of item in suggestions list
+    const [activeIndex, setActiveIndex] = useState<number>(-1);
+
+    // handle when clicking to list item (single address)
+    const handleSelect = async (address: string) => {
+        setValue(address, false); // set current address to user input
+        clearSuggestions(); // clear suggestions list
+
+        const results = await getGeocode({address});
+        const {lat, lng} = await getLatLng(results[0]);
+        
+        if (props.inputStyle === "origin")
+        {
+            dispatch(callReceiptHandlerActions.updateOriginGeolocation({
+                lat: lat,
+                lng: lng
+            }))
+        }
+        
+        else if (props.inputStyle === 'destination') {
+            dispatch(callReceiptHandlerActions.updateDestinationGeolocation({
+                lat: lat,
+                lng: lng
+            }))
+        }
+    };
+
+    // handle when moving key down
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "ArrowDown") {
+            event.preventDefault();
+            setActiveIndex((prevIndex: number) => Math.min(prevIndex + 1, data.length - 1));
+        }
+        else if (event.key === "ArrowUp") {
+            event.preventDefault();
+            setActiveIndex((prevIndex: number) => Math.max(prevIndex - 1, -1));
+        }
+        else if (event.key === "Enter") {
+            event.preventDefault();
+            const {place_id, description} = data[activeIndex];
+            handleSelect(description);
+        }
+    }
+
+    // handle when input is changed
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value, true);
+        setActiveIndex(-1);
+    }
+
+
     return <div className={styles["places-complete-input"]}>
-        <input value = {value} disabled = {!ready} onChange={(e) => setValue(e.target.value)} type = "text" placeholder = {props.inputStyle === "origin" ? "Nhập địa chỉ đón..." : "Nhập địa chỉ đến..."} />
+        <input value = {value} disabled = {!ready} onChange = {handleInputChange}
+        type = "text" placeholder = {props.inputStyle === "origin" ? "Nhập địa chỉ đón..." : "Nhập địa chỉ đến..."} 
+        onKeyDown = {handleKeyPress}
+        />
         {status === 'OK' && 
         <div className = {styles["suggestion-list"]}>
             {data.map(({place_id, description}, index) => (
-
-            // <li style = {{listStyle: "none", backgroundColor: "red", fontSize: "1.5rem", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis",
-            // padding: "1rem", borderBottom: "1px solid black", cursor: "pointer"
-            // }} 
+            // <li className = {styles["suggestion-item"]}
             // key={place_id} value = {description} onClick={() => handleSelect(description)}>
             //     {description}
             // </li>
-
-            <li className = {styles["suggestion-item"]}
-            key={place_id} value = {description} onClick={() => handleSelect(description)}>
-                {description}
+            <li className = {activeIndex === index ? `${styles["suggestion-item"]} ${styles["active"]}` : styles["suggestion-item"]} 
+            key={place_id} 
+            value = {description} 
+            onClick={() => handleSelect(description)}
+            >
+                <span className={styles["location-icn"]}>
+                    <LocationIcon/>
+                </span>
+                <span className={styles["description"]}>
+                  {description}
+                </span>
             </li>
         ))}
         </div>}            
     </div> 
 }
-
 export default PlacesAutocomplete;
 
 
