8be4e8d0d48c5aeb517afaa28ded8b8b1b4802c3
khangduy017 <khangduy017@gmail.com>
2023-09-08 18:36:10 +0700

    handle booking history

---
+++ servers/auth/common/controllers/customerAuth.controller.ts
@@ -0,0 +1,119 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import Controller from '@common/interfaces/controller';
+import validationMiddleware from '@common/middlerware/validation.middleware';
+import UserLoginDto from '@common/dtos/userLogin.dto';
+import JsonWebToken from '@common/utils/jwt';
+import catchAsync from '@common/utils/catchAsync';
+import AppError from '@common/utils/appError';
+import { promisify } from 'util';
+import UserRegistrationDto from '@common/dtos/userRegistration.dto';
+import customerModel from '@common/models/customer.model';
+import bookingHistory from '@common/models/booking_history.model';
+
+/*
+    AUTH CONTROLLER
+    1. login
+    2. register
+    3. forgot password
+    4. reset password
+*/
+
+const jwt = new JsonWebToken(process.env.JWT_SECRET);
+
+class customerAuthController implements Controller {
+    path: string = '/v1/customer-auth';
+    router: Router = Router();
+
+    constructor() {
+        this.router.post('/login', this.login);
+        this.router.post('/register', this.register);
+    }
+
+    /// > LOGIN
+    private login = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        // get phone number, password of user
+        console.log('req.body.user: ', req.body.email);
+        const customer = await customerModel.findOne({ email: req.body.email });
+        if (customer != null) {
+            const historyList = await bookingHistory.find({ customer: customer?._id.toString() });
+            console.log(historyList);
+
+            return res.status(200).json({
+                status: 'success',
+                info: JSON.stringify(customer),
+                history: historyList.reverse(),
+                // accessToken: accessToken,
+            });
+        } else {
+            customerModel.create({
+                name: req.body.name,
+                email: req.body.email,
+                phonenumber: req.body.phonenumber,
+            });
+            return res.status(200).json({
+                status: 'success',
+                info: JSON.stringify({
+                    name: req.body.name,
+                    email: req.body.email,
+                    phonenumber: req.body.phonenumber,
+                }),
+                history: [],
+                // accessToken: accessToken,
+            });
+        }
+    });
+
+    /// > REGISTER
+    private register = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        // get phone number, password, password confirm from client
+        const { phone_number, password, password_confirm } = req.body as UserRegistrationDto;
+        // validate the password
+        const userRegistrationDto = new UserRegistrationDto(phone_number, password, password_confirm);
+        if (!userRegistrationDto.comparePasswords())
+            return next(new AppError('Password does not match. Try again!', 400));
+
+        // return message
+        return res.status(200).json({
+            status: 'success',
+            message: 'Register successfully',
+        });
+    });
+
+    /// > FORGOT PASSWORD
+    private forgotPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        res.status(200).json({
+            status: 'success',
+            message: 'reset password successfully',
+        });
+    });
+
+    /// > PROTECT
+    private protect = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        // 1) Getting token and check of it's there
+        let token = '';
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
+            token = req.headers.authorization.split('')[1];
+
+        if (!token) {
+            return next(new AppError('You are not logged in! Please log in to get access', 401));
+        }
+
+        // 2) Verification token
+        const decoded = await promisify(jwt.verifyToken);
+
+        // 3) Check if user still exists
+        // const currentUser = await User.findById(decoded.id);
+        // if (!currentUser) return next(new AppError('The user belonging to this token doest no longer exist.', 401));
+
+        // 4) Check if user changed password after the token was issued
+        // if (currentUser.changedPasswordAfter(decoded.iat))
+        // return next(new AppError('User recently changed password! Please log in again.'), 401);
+
+        // GRANT ACCESS TO PROTECT ROUTE
+        // req.user = currentUser;
+        // next();
+    });
+}
+
+export default customerAuthController;
+
--- servers/auth/server.ts
+++ servers/auth/server.ts
@@ -1,12 +1,13 @@
 import Application from '@common/app';
 import UserController from '@common/controllers/auth.controller';
+import CustomerController from '@common/controllers/customerAuth.controller';
 import dotenv from 'dotenv';
 
 process.on('uncaughtException', (err: Error) => {
     console.error('Uncaught Exception. Shutting down...');
     console.error(err.name, err.message, err.stack);
 
-    setTimeout(() => { 
+    setTimeout(() => {
         process.exit(1);
     }, 3000);
 });
@@ -14,7 +15,7 @@ process.on('uncaughtException', (err: Error) => {
 dotenv.config({ path: './.env.local' });
 
 const app = new Application({
-    controllers: [new UserController()],
+    controllers: [new UserController(), new CustomerController()],
     mongoConnection: {
         uri: process.env.MONGO_URI as string,
     },
@@ -25,10 +26,11 @@ const server = app.run(4100);
 process.on('unhandledRejection', (err: Error) => {
     console.error('Unhandled Rejection. Shutting down...');
     console.error(err.name, err.message, err.stack);
-    
-    setTimeout(() => { 
+
+    setTimeout(() => {
         server.close(() => {
             process.exit(1); // 0 is success, 1 is uncaught exception
         });
     }, 3000);
 });
+
