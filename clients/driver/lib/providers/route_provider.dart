82ff894849c5db39e8d27a60d5e5ebcdcdb6a5e8
NG-MinG <wgmin.it@outlook.com>
2023-09-11 21:20:47 +0700

    update smooth position

--- clients/driver/lib/providers/route_provider.dart
+++ clients/driver/lib/providers/route_provider.dart
@@ -5,12 +5,16 @@ class RouteDirection {
   final LocationPostion startLocation;
   final String instruction;
   final String maneuver;
+  final num duration;
+  final num distance;
   final int index;
 
   RouteDirection(
       {required this.startLocation,
       required this.instruction,
-      required this.maneuver, 
+      required this.maneuver,
+      required this.duration,
+      required this.distance,
       required this.index});
 
   factory RouteDirection.fromJson(Map<String, dynamic> json) {
@@ -18,6 +22,8 @@ class RouteDirection {
       startLocation: LocationPostion.fromJson(json['start_location']),
       instruction: json['instruction'],
       maneuver: json['maneuver'],
+      duration: json['duration'],
+      distance: json['distance'],
       index: json['index'],
     );
   }
@@ -26,6 +32,8 @@ class RouteDirection {
         'start_location': startLocation.toJson(),
         'instruction': instruction,
         'maneuver': maneuver,
+        'duration': duration,
+        'distance': distance,
       };
 }
 
@@ -34,11 +42,16 @@ class RouteProvider extends StateNotifier<RouteDirection> {
       : super(RouteDirection(
             startLocation: LocationPostion(latitude: 0, longitude: 0),
             instruction: '',
-            maneuver: '', index: -1));
+            maneuver: '',
+            duration: 0,
+            distance: 0,
+            index: -1));
 
   void setRoute(RouteDirection route) {
     state = route;
   }
+
+  bool get hasValue => state.index != -1;
 }
 
 final routeProvider = StateNotifierProvider<RouteProvider, RouteDirection>(
