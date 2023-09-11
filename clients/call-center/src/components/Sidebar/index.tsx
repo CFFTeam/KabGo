93f8591a7068172159361b8c1d87315f229eb863
Gia Huy <giahuy200202@gmail.com>
2023-09-11 11:23:34 +0700

    add toast warning with wrong account

--- clients/call-center/src/components/Sidebar/index.tsx
+++ clients/call-center/src/components/Sidebar/index.tsx
@@ -6,8 +6,19 @@ import { sidebarActions } from "@store/reducers/sidebarSlice";
 import { NavLink, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { ReactComponent as FooterKabgo } from "@assets/svg/Sidebar/footerLogo.svg";
+import { authStorage } from "@utils/storage";
+import toast, { Toaster } from "react-hot-toast";
 
 const Sidebar: React.FC = () => {
+  const styleError = {
+    style: {
+      border: "2px solid red",
+      padding: "10px",
+      color: "red",
+      fontWeight: "500",
+    },
+    duration: 4000,
+  };
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   let location = useLocation();
@@ -32,16 +43,36 @@ const Sidebar: React.FC = () => {
     }
   }, [location.pathname]);
   
+  const handleMainMenuClick = (link: string, index: number) => {
+    if(link==="/dashboard"){
+      if(authStorage.getAuthData().role!=="Supervisor"){
+        toast.error("Tài khoản không có quyền truy cập. Vui lòng chọn tài khoản khác.", styleError);
+      } 
+      else{
+        onChangeActive([index, "main-menu"]);
+      }
+    }
+    if(link==="/call-receipt-handle"){
+      if(authStorage.getAuthData().role!=="Coordinator"){
+        toast.error("Tài khoản không có quyền truy cập. Vui lòng chọn tài khoản khác.", styleError);
+      }
+      else{
+        onChangeActive([index, "main-menu"]);
+      }
+    }
+    // navigate(link);
+    
+  }
   return (
     <div className={styles["sidebar-container"]}>
       <KabGoIcon className={styles["logo"]} />  
       <div className={styles["sidebar-title"]}>Main Menu</div>
       <div className={styles["sidebar-sub-container"]}>
         {mainMenuData.map((data, index) => (
-            <NavLink to= {data.link} onClick={()=>onChangeActive([index, "main-menu"])} className={`${styles["sidebar-el-container"]} ${data.active ? styles["change-border-color"] : ''}`} key={index}>
+            <div onClick={()=>handleMainMenuClick(data.link, index)} className={`${styles["sidebar-el-container"]} ${data.active ? styles["change-border-color"] : ''}`} key={index}>
               <div className={styles["sidebar-img"]}> <img src={data.active ? data.imgFill : data.img} /></div>
               <div className={`${styles["sidebar-content-arrow"]} ${data.active ? styles["change-text-color"] : ''}`}>{data.name}</div>
-            </NavLink>
+            </div>
           )
         )}  
       </div>
@@ -57,6 +88,7 @@ const Sidebar: React.FC = () => {
         )}
       </div>
       <FooterKabgo className={styles["sidebar-footer-logo"]}/>
+      <Toaster position="top-right" />
     </div>
   );
 };
