704f9904eee1bbdec646610ba57ebe7a9b9d0fd7
NG-MinG <wgmin.it@outlook.com>
2023-09-12 17:32:49 +0700

    fix conflict

--- clients/driver/lib/.env.dart
+++ clients/driver/lib/.env.dart
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-const String googleAPIKey = 'AIzaSyA6D_kw5-6l1MjqwwTaSdyo7RW-1aqKDtc';
-const String ip = '192.168.2.5';
-=======
-const String googleAPIKey = 'AIzaSyAkYoeBAUQ0mANU3hZSQG_hr_3xOzrjdaU';
-const String ip = '192.168.1.13';
->>>>>>> fedefdd69132ec7ec3e8f6b879dd17c9c7a40ab4
+const String googleAPIKey = 'AIzaSyDQVARJoKH_nW1MOxPH8zRfZ9Uyz5CoiWo';
+const String ip = '192.168.1.244';
